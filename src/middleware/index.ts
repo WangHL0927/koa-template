b518a9abd2e0c6ff1ac9cf9ffbc9946ef147202a
WangHualong <whl958660278@gmail.com>
2020-09-04 13:25:06 +0800

    add koa compress.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,14 +1,28 @@
 import cors from '@koa/cors';
 import koaBody from 'koa-body';
-import koaLogger from 'koa-logger'
+import koaCompress from 'koa-compress';
+import koaLogger from 'koa-logger';
+import zlib from "zlib";
 
 import { createLogger } from '../extend/logger';
-import router from '../router'
+import router from '../router';
 
 export default function initMiddleware(app) {
-  const reqLogger = createLogger('koa')
+  const reqLogger = createLogger('koa');
   app.use(cors());
   app.use(koaLogger(str => reqLogger.info(str)));
+  app.use(koaCompress({
+    threshold: 2048,
+    gzip: {
+      flush: zlib.constants.Z_SYNC_FLUSH
+    },
+    deflate: {
+      flush: zlib.constants.Z_SYNC_FLUSH,
+    },
+    br: {
+      flush: zlib.constants.BROTLI_OPERATION_FLUSH,
+    }
+  }));
   app.use(koaBody());
   app.use(router.routes());
   app.use(router.allowedMethods());
